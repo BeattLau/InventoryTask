99c423ad15f91086ae9f22e4db9f1c1a11a9aaba
Laura Beattie <laurabeattie@lauras-air.speedport.ip>
2023-10-10 17:28:44 +0200

    Updated inventory class to be more streamlined

--- InventoryManagementSystem/Inventory.java
+++ InventoryManagementSystem/Inventory.java
@@ -26,25 +26,28 @@ public class Inventory {
             throw new ItemNotFoundException("Item not found");
         }
         return items.get(index);
-
     }
 
-    public int insufficientStock(Item item) throws ItemNotFoundException,InsufficientStockException {
-            int index = items.indexOf(item);
-            if (index < 0) {
-                throw new ItemNotFoundException("Item not found");
-            }
-
-            int quantity = items.get(index).getQuantity();
-
-            if (quantity <= 0) {
-                throw new InsufficientStockException("Insufficient items in stock");
-            }
-
-            return quantity;
+    public int insufficientStock(Item item) throws ItemNotFoundException, InsufficientStockException {
+        int index = items.indexOf(item);
+        if (index < 0) {
+            throw new ItemNotFoundException("Item not found");
         }
 
+        int quantity = items.get(index).getQuantity();
+
+        if (quantity <= 0) {
+            throw new InsufficientStockException("Insufficient items in stock");
+        }
+
+        return quantity;
     }
+}
+
+
+
+
+
 
 
 
