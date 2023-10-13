a5f31695b9912c6df19a4519b5d6b7d0f396c046
Laura Beattie <laurabeattie@lauras-air.speedport.ip>
2023-10-13 15:31:59 +0200

    removed files from main after adding to laura branch

--- InventoryManagementSystem/InsufficientStockException.java
+++
@@ -1,7 +0,0 @@
-package InventoryManagementSystem;
-
-public class InsufficientStockException extends Exception{
-    public InsufficientStockException(String message){
-        super (message);
-    }
-}
