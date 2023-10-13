df4f067b2afd612fdb9549df823fc6ae418def3d
Laura Beattie <laurabeattie@lauras-air.speedport.ip>
2023-10-13 15:05:03 +0200

    Added test classes and updated pom

---
+++ InventoryManagementSystem/src/test/java/InventoryManagementSystem/InventoryTest.java
@@ -0,0 +1,32 @@
+package InventoryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryTest {
+
+    @Test
+    void removeItem() {
+    }
+
+    @Test
+    void decreaseItemQuantity() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void insufficientStock() {
+    }
+}
---
+++ InventoryManagementSystem/src/test/java/InventoryManagementSystem/ItemTest.java
@@ -0,0 +1,32 @@
+package InventoryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getQuantity() {
+    }
+
+    @Test
+    void setQuantity() {
+    }
+}
